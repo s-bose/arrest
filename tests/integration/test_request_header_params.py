f20e97a51403bdb549978e1e6796f1936e9c0944
shiladitya <shiladitya_basu@live.com>
2024-10-31 13:05:41 +0530

    refactor some tests

--- tests/integration/test_request_header_params.py
+++ tests/integration/test_request_header_params.py
@@ -98,3 +98,75 @@ async def test_request_header_params(service, mock_httpx, resource_header, kwarg
     for key, value in expected_result.items():
         assert key in headers
         assert headers[key] == value
+
+
+@pytest.mark.asyncio
+async def test_request_header_params_arguments(service, mock_httpx):
+    mock_httpx.route(url__regex="/user/*", name="http_request", method__in=["GET", "POST"]).mock(
+        return_value=httpx.Response(200, json={"status": "OK"})
+    )
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.GET, "/profile"),
+            ],
+        )
+    )
+
+    await service.user.get("/profile", headers={"x-header": "123"})
+    request: httpx.Request = mock_httpx["http_request"].calls[0].request
+
+    request_headers = httpx.Headers(request.headers)
+    assert request_headers["x-header"] == "123"
+
+
+@pytest.mark.asyncio
+async def test_header_params_in_both_request_model_and_arguments(service, mock_httpx):
+    patterns = [
+        M(url__regex="/user/*", method__in=["GET"]),
+    ]
+
+    mock_httpx.route(*patterns, name="http_request").mock(
+        return_value=httpx.Response(200, json={"status": "OK"})
+    )
+
+    class UserRequest(BaseModel):
+        if PYDANTIC_V2:
+            x_user_agent: str = Header(serialization_alias="x-user-agent")
+        else:
+            x_user_agent: str = Header(alias="x-user-agent")
+
+        name: str
+        email: str
+        password: str
+
+        if PYDANTIC_V2:
+            model_config = ConfigDict(populate_by_name=True)
+
+        else:
+
+            class Config:
+                allow_population_by_field_name = True
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.GET, "/profile", UserRequest),
+            ],
+        )
+    )
+
+    await service.user.get(
+        "/profile",
+        request=UserRequest(x_user_agent="mozila", name="abc", email="abc@email.com", password="123"),
+        headers={"x-header": "123"},
+    )
+
+    request: httpx.Request = mock_httpx["http_request"].calls[0].request
+
+    request_headers = httpx.Headers(request.headers)
+    assert request_headers["x-header"] == "123"
+    assert request_headers["x-user-agent"] == "mozila"
--- tests/integration/test_request_query_params.py
+++ tests/integration/test_request_query_params.py
@@ -1,13 +1,11 @@
 import httpx
 import pytest
-import respx
 from pydantic import BaseModel, ValidationError
 from respx.patterns import M
 
 from arrest.http import Methods
 from arrest.params import Query
 from arrest.resource import Resource
-from tests import TEST_DEFAULT_SERVICE_URL
 
 
 @pytest.mark.asyncio
@@ -68,24 +66,95 @@ async def test_request_query_params_invalid_type(service, mocker):
 
 @pytest.mark.asyncio
 async def test_request_query_params_validation_error(service):
-    with respx.mock(base_url=TEST_DEFAULT_SERVICE_URL, assert_all_called=False) as mock:
-        mock.route(url__regex="/user/*", name="http_request", method__in=["GET", "POST"]).mock(
-            return_value=httpx.Response(200, json={"status": "OK"})
+    class UserRequest(BaseModel):
+        limit: int = Query(gt=10)
+        q: str = Query(default="default")
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.POST, "/profile", UserRequest),
+            ],
         )
+    )
 
-        class UserRequest(BaseModel):
-            limit: int = Query(gt=10)
-            q: str = Query(default="default")
+    with pytest.raises(ValidationError):
+        await service.user.post("/profile", request=UserRequest(limit=15, q=123.0))
+        await service.user.post("/profile", request=UserRequest(limit=10, q="abc"))
 
-        service.add_resource(
-            Resource(
-                route="/user",
-                handlers=[
-                    (Methods.POST, "/profile", UserRequest),
-                ],
-            )
+
+@pytest.mark.asyncio
+async def test_request_query_params_in_url(service, mock_httpx):
+    mock_httpx.route(url__regex="/user/*", name="http_request", method__in=["GET", "POST"]).mock(
+        return_value=httpx.Response(200, json={"status": "OK"})
+    )
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.GET, "/profile"),
+            ],
         )
+    )
 
-        with pytest.raises(ValidationError):
-            await service.user.post("/profile", request=UserRequest(limit=15, q=123.0))
-            await service.user.post("/profile", request=UserRequest(limit=10, q="abc"))
+    await service.user.get("/profile?limit=2&q=abc")
+    request: httpx.Request = mock_httpx["http_request"].calls[0].request
+    assert httpx.QueryParams(request.url.query) == httpx.QueryParams({"limit": "2", "q": "abc"})
+
+
+@pytest.mark.asyncio
+async def test_request_query_params_in_arguments(service, mock_httpx):
+    mock_httpx.route(url__regex="/user/*", name="http_request", method__in=["GET", "POST"]).mock(
+        return_value=httpx.Response(200, json={"status": "OK"})
+    )
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.GET, "/profile"),
+            ],
+        )
+    )
+
+    await service.user.get("/profile", query={"limit": 2, "q": "abc"})
+    request: httpx.Request = mock_httpx["http_request"].calls[0].request
+
+    assert httpx.QueryParams(request.url.params) == httpx.QueryParams({"limit": 2, "q": "abc"})
+
+
+@pytest.mark.asyncio
+async def test_query_params_in_both_request_model_and_arguments(service, mock_httpx, mocker):
+    patterns = [
+        M(url__regex="/user/*", method__in=["GET"]),
+    ]
+
+    mock_httpx.route(*patterns, name="http_request").mock(
+        return_value=httpx.Response(200, json={"status": "OK"})
+    )
+
+    class UserRequest(BaseModel):
+        limit: int = Query(...)
+        name: str
+        email: str
+        password: str
+
+    service.add_resource(
+        Resource(
+            route="/user",
+            handlers=[
+                (Methods.GET, "/profile"),
+            ],
+        )
+    )
+
+    await service.user.get(
+        "/profile",
+        request=UserRequest(limit=2, name="abc", email="abc@email.com", password="123"),
+        query={"value": "xyz"},
+    )
+
+    request: httpx.Request = mock_httpx["http_request"].calls[0].request
+    assert httpx.QueryParams(request.url.params) == httpx.QueryParams({"limit": 2, "value": "xyz"})
